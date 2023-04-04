9447959865735c7e246e504d67b50a0931fb1d5c
Wania-Kazmi <waniakz019@gmail.com>
2023-04-05 01:20:53 +0500

    added bookDetail page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Home from "@/views/Home"
 export default function App() {
   return (
     <div>
+      {/* @ts-expect-error Server Component*/}
       <Home />
     </div>
   )
--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -1,75 +1,47 @@
 import React from "react";
+import { API_BASE_URL } from "@/utils";
+import Link from "next/link";
 
-export default function Home() {
+//server data fetching to get books List
+async function getBooksList() {
+  const res = await fetch(`${API_BASE_URL}/books`);
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+  return res.json();
+}
+
+interface BookItems {
+  id: number;
+  name: string;
+  type: string;
+  available: boolean;
+}
+
+export default async function Home() {
+  const books = await getBooksList();
+  console.log(books);
   return (
     <div>
       <div className="container grid sm:grid-cols-2 md:grid-cols-3 gap-x-8 gap-y-8">
-        <div className="item">
-          <img src="https://picsum.photos/400/300" alt="" />
-          <div>
-            <h2>Blog title 1</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-              assumenda porro inventore repellendus ipsum.
-            </p>
-            <a href="#">Read more</a>
-          </div>
-        </div>
-        <div className="item">
-          <img src="https://picsum.photos/400/300?1" alt="" />
-          <div>
-            <h2>Blog title 2</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-              assumenda porro inventore repellendus ipsum.
-            </p>
-            <a href="#">Read more</a>
-          </div>
-        </div>
-        <div className="item">
-          <img src="https://picsum.photos/400/300?2" alt="" />
-          <div className="desc">
-            <h2>Blog title 3</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-              assumenda porro inventore repellendus ipsum.
-            </p>
-            <a href="#">Read more</a>
-          </div>
-        </div>
-        <div className="item">
-          <img src="https://picsum.photos/400/300?3" alt="" />
-          <div className="desc">
-            <h2>Blog title 4</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-              assumenda porro inventore repellendus ipsum.
-            </p>
-            <a href="#">Read more</a>
-          </div>
-        </div>
-        <div className="item">
-          <img src="https://picsum.photos/400/300?4" alt="" />
-          <div className="desc">
-            <h2>Blog title 5</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-              assumenda porro inventore repellendus ipsum.
-            </p>
-            <a href="#">Read more</a>
-          </div>
-        </div>
-        <div className="item">
-          <img src="https://picsum.photos/400/300?5" alt="" />
-          <div className="desc">
-            <h2>Blog title 6</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-              assumenda porro inventore repellendus ipsum.
-            </p>
-            <a href="#">Read more</a>
-          </div>
-        </div>
+        {books?.map((item: BookItems) => (
+          <Link href={`bookDetail/${item.id}`} key={item.id}>
+            <div className="item" key={item.id}>
+              <img src="https://picsum.photos/400/300" alt="" />
+              <div>
+                <h2>{item.name}</h2>
+                <p>
+                  <span className="font-bold">Type:</span> {item.type}
+                </p>
+                <p>
+                  <span className="font-bold">In Stock:</span>{" "}
+                  {item.available === true ? "Available" : "Not Available"}
+                </p>
+                {/* <a href="#">Read more</a> */}
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
