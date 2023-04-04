dd21c90e6337889ae434e3302448a1314cd93129
Wania-Kazmi <waniakz019@gmail.com>
2023-04-05 01:28:06 +0500

    update name of page bookDetail

--- app/BookDetail/[id]/page.tsx
+++ app/BookDetail/[id]/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default function BookDetail() {
   return (
-    <div>BookDetail</div>
+    <div> BookDetail is this  </div>
   )
 }
--- views/Header/index.tsx
+++ views/Header/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  return( <div className="fixed w-full flex justify-between items-center bg-white py-2 px-4 border border-b-[#e6ebf4] ">
+  return( <div className="w-full flex justify-between items-center bg-white py-2 px-4 border border-b-[#e6ebf4] ">
     <Link href={"/"} className="text-lg font-bold text-purple-600">Book API</Link>
     <Link href={"/register"} className="bg-[#6469ff] font-medium text-white py-2 px-4 rounded-md">Register</Link>
   </div>
